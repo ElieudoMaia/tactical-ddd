63319a2a0a5cb943f7f8e2c248031e5428a8efdb
Elieudo Maia <elieudo.maia@g4flex.com.br>
2022-09-17 19:20:25 -0300

    feat: add business rules examples

--- src/entity/costumer.ts
+++ src/entity/costumer.ts
@@ -1,22 +1,42 @@
 class Costumer {
-  public _isActive: boolean = true;
+  private _id: string = "";
+  private _name: string = "";
+  private _address: string = "";
+  private _isActive: boolean = true;
 
-  constructor(
-    public _id: string,
-    public _name: string,
-    public _address: string
-  ) {}
-
-  changeName(name: string) {
-    if (name.length > 255) throw new Error("Name is too long");
+  constructor(id: string, name: string) {
+    this._id = id;
     this._name = name;
+    this.validate();
   }
 
-  activate() {
+  private validate() {
+    this.validateID();
+    this.validateName();
+  }
+
+  private validateID() {
+    if (!this._id) throw new Error("ID is required");
+  }
+
+  private validateName() {
+    if (!this._name) throw new Error("Name is required");
+    if (this._name.length > 255) throw new Error("Name is too long");
+  }
+
+  public changeName(name: string) {
+    this._name = name;
+    this.validateName();
+  }
+
+  public activate() {
+    if (!this._address) {
+      throw new Error("Address is required to activate the costumer");
+    }
     this._isActive = true;
   }
 
-  deactivate() {
+  public deactivate() {
     this._isActive = false;
   }
 }
